69a2a4af19a152d4f7a978cc0de00a7a93a678c8
Yashraj Shrivastava <yashraj.shri4@gmail.com>
2026-07-07 17:39:55 +0530

    Time: 16 ms (14.36%) | Memory: 46.9 MB (16.54%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -10,6 +10,6 @@ class Solution {
         String reversed = new StringBuilder(str).reverse().toString();
 
         
-        return str.equals(reversed);
+        return str.equals(reversed); 
     }
 }
